f95c35ef96092568e31ce4a62a25b2832b17d1ab
vrajpatell <vrajpatell0712@gmail.com>
2021-03-24 12:15:44 -0500

    Task 5 Commit updated files and added files

--- Task5.py
+++ Task5.py
@@ -20,18 +20,18 @@ fun1(n)
 # entered name is incorrect throw an exception and ask them to enter the name again. Make sure
 # to use read only mode.
 
-try:
-    with open(sys.argv[1], 'r') as file1:
-        print(file1.read())
-except FileNotFoundError:
-    print("Enter Valid name of File")
+# try:
+#   with open(sys.argv[1], 'r') as file1:
+#      print(file1.read())
+# except FileNotFoundError:
+#   print("Enter Valid name of File")
 
 # 3. Write a program to handle an error if the user entered a number more than four digits it should
 # return “The length is too short/long !!! Please provide only four digits”
 
 while True:
     try:
-        num1 = int(input("enter number"))
+        num1 = input("enter number")
         if (len(num1) == 4):
             print("enter number is four digit only")
             break
@@ -58,4 +58,36 @@ while count < 4:
     except Exception:
         print("Please try again")
 
+# 6. Read doc.txt file using Python File handling concept and return only the even length string from
+# the file. Consider the content of doc.txt as given below:
+# Hello I am a file
+# Where you need to return the data string
+# Which is of even length
+# Make sure you return the content in The same link as it is present.
 
+print('reading characters of files')
+char_list = []
+with open('demo.txt', "r") as f:
+    for line in f:
+        char_list.extend(line.split())
+
+# print(char_list)
+store_char = []
+
+
+def evenLenght(s):
+    for word in s:
+        if len(word) % 2 == 0:
+            store_char.append(word)
+
+
+evenLenght(char_list)
+print(store_char)
+
+with open('evenDemo.txt', 'w') as file1:
+    for listitem in store_char:
+        file1.write('%s\n' % listitem)
+
+file1 = open('evenDemo.txt', 'r')
+print(file1.read())
+file1.close()
